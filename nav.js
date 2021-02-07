34ec93234fe81317e13782d0fdb140e81b04ab6c
biggant1 <meme34903@gmail.com>
2021-02-07 13:39:06 -0800

    mobile navbar

--- nav.js
+++ nav.js
@@ -1,8 +1,4 @@
-document.addEventListener('touchstart', (event) => {
-    let dropdown = document.querySelector('.dropdown-content');
-    if(event.target.className.startsWith('dropdown')) {
-        dropdown.style.display = "block"
-    } else if(window.getComputedStyle(dropdown, null).display === "block") {
-        dropdown.style.display = "none"
-    }
-})
+function togglePop(x) {
+    x.classList.toggle("change");
+    document.querySelector('#box').classList.toggle("boxOn")
+}
